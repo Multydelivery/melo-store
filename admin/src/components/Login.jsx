feb471c359d98a99654af2a63d2d02ac072b6126
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-21 16:53:29 -0500

    l

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -11,8 +11,7 @@ const Login = ({setToken}) => {
     const onSubmitHandler = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.post(backendUrl + '/api/user/admin',{email,password}, {headers: {token}}
-            )
+            const response = await axios.post(backendUrl + '/api/user/admin',{email,password})
             console.log(response.data);
             if (response.data.success) {
                 setToken(response.data.token)
