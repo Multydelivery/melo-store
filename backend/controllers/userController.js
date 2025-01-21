f3c94cb45086e031af573f1cc2e02e489a1bd580
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-21 16:12:10 -0500

    modified Admin login route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,24 +88,25 @@ const registerUser = async (req, res) => {
 
 }
 
-// Route for admin login
-
+// Route for Admin login
 const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
 
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            // Assuming jwt.sign() returns a Promise
-            const token = jwt.sign({email: email}, process.env.JWT_SECRET);
+            const token = jwt.sign(email+password, process.env.JWT_SECRET)
             res.json({ success: true, token });
+
         } else {
             res.json({ success: false, message: "Invalid credentials" });
         }
+
+
+
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
     }
 }
-
 
 export { loginUser, registerUser, adminLogin };
