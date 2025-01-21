13723e40c1146cfcbfea3517a5cbb3b1cedfe2b9
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-20 21:24:31 -0500

    l

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from 'cors'
+
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
@@ -16,7 +16,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+
 
 // api endpoints
 app.use('/api/user',userRouter)
